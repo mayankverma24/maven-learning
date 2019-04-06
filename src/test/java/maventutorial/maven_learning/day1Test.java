51db3d57bce263c4d3eec5c48f0db9896b99688a
mayankverma786 <mayankverma786@gmail.com>
2019-04-06 19:31:53 +0530

    k

---
+++ src/test/java/maventutorial/maven_learning/day1Test.java
@@ -0,0 +1,18 @@
+package maventutorial.maven_learning;
+
+import org.testng.annotations.Test;
+
+public class day1Test {
+
+	@Test
+	public void carLoan()
+	{
+		System.out.println("carLoan");
+	}
+	
+	@Test
+	public void homeLoan()
+	{
+		System.out.println("homeLoan");
+	}
+}
---
+++ src/test/java/maventutorial/maven_learning/day2Test.java
@@ -0,0 +1,18 @@
+package maventutorial.maven_learning;
+
+import org.testng.annotations.Test;
+
+public class day2Test {
+
+	@Test
+	public void carLoan1()
+	{
+		System.out.println("carLoan1");
+	}
+	
+	@Test
+	public void homeLoan1()
+	{
+		System.out.println("homeLoan1");
+	}
+}
